3a7d96c303ef14e18ef4b09098d9254cf6865333
Jorge Escobar <jorge@fromzero.io>
2018-05-24 08:10:10 -0400

    settings updates

--- .flaskenv
+++ .flaskenv
@@ -1,7 +1,7 @@
 FLASK_APP='manage.py'
 FLASK_ENV=development
 SECRET_KEY='my_secret_key'
-DB_HOST='127.0.0.1'
+DB_HOST=localhost
 DB_USERNAME='counter_app'
 DB_PASSWORD='mypassword'
 DATABASE_NAME='counter'
--- settings.py
+++ settings.py
@@ -3,7 +3,6 @@ import os
 SECRET_KEY = os.getenv('SECRET_KEY')
 DB_USERNAME=os.environ['DB_USERNAME']
 DB_PASSWORD=os.environ['DB_PASSWORD']
-MYSQL_ROOT_PASSWORD=os.environ['MYSQL_ROOT_PASSWORD']
 DB_HOST=os.environ['DB_HOST']
 DATABASE_NAME=os.environ['DATABASE_NAME']
 DB_URI = "mysql+pymysql://%s:%s@%s:3306/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST, DATABASE_NAME)
