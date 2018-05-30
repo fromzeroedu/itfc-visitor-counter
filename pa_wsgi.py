7449a8d8544900b02592d37046273fcff991eab2
Jorge Escobar <jorge@fromzero.io>
2018-05-30 08:59:09 -0400

    Added pa_wsgi

---
+++ pa_wsgi.py
@@ -0,0 +1,15 @@
+import os, sys
+from dotenv import load_dotenv
+
+python_anywhere_username = 'jorge3'
+path = '/home/' + python_anywhere_username + '/opt/visitor-counter'
+if path not in sys.path:
+    sys.path.append(path)
+
+load_dotenv(os.path.join(path, '.flaskenv'))
+
+from hello import app
+app.debug = True
+
+from werkzeug.debug import DebuggedApplication
+application = DebuggedApplication(app, evalex=True)
