5e54ac4a2f1eb7f684a66031121919f51b961dc6
Jorge Escobar <jorge@fromzero.io>
2018-06-08 17:28:32 -0400

    step-5

--- counter/views.py
+++ counter/views.py
@@ -6,4 +6,12 @@ counter_app = Blueprint('counter_app', __name__)
 
 @counter_app.route('/')
 def init():
-	return 'Counter App'
+    counter = Counter.query.first()
+    if not counter:
+        counter = Counter(1)
+        db.session.add(counter)
+        db.session.commit()
+    else:
+        counter.count += 1
+        db.session.commit()
+    return "<h1>Counter: " + str(counter.count) + "</h1>"
